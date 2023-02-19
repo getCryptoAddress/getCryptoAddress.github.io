7d5559b462bd7ec5a8ad77a60c8f092b32ad955d
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2023-02-19 05:08:45 +0200

    add FormCreateWallets

---
+++ src/components/FormCreateWallets/FormCreateWallets.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import FormCreateWallets from "./FormCreateWallets.vue";
+</script>
+
+<template>
+  <Story>
+    <FormCreateWallets />
+  </Story>
+</template>
---
+++ src/views/CreateWalletsView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import FormCreateWallets from "@/components/FormCreateWallets/FormCreateWallets.vue";
+import { usePrivateKeysStore } from "@/stores/privateKeys";
+// import { useAddressStore } from "@/stores/address";
+
+const privateKeysStore = usePrivateKeysStore();
+
+// const addressStore = useAddressStore();
+
+function handleForm({ count }: { count: number }) {
+  privateKeysStore.generatePrivatesKeys(count);
+}
+</script>
+
+<template>
+  <FormCreateWallets @submit="handleForm" />
+
+  WIP code:
+  <pre>
+  {{ privateKeysStore.formattedPrivateKeys }}
+  </pre>
+  <pre></pre>
+</template>
