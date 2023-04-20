ab90d378a40e9d7721ab762e409e810d9dec98c0
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2023-04-21 02:58:42 +0300

    update prerender routers

--- prerender.mjs
+++ prerender.mjs
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const routes = ["/", "/create-wallet"];
+const routes = ["/", "/create-wallet", "/create-paper-wallet"];
 
 import { createServer } from "vite";
 
