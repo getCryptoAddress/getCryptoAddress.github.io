7ae2b9cdb0adda21f668fb505829b9ece36bc049
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2023-04-21 02:09:21 +0300

    make form with rules for PaperWalletsView

--- src/views/PaperWalletsView.vue
+++ src/views/PaperWalletsView.vue
@@ -1,44 +1,107 @@
 <script lang="ts" setup>
 import { ref } from "vue";
-import { NDivider, NInput, NSpace } from "naive-ui";
+import {
+  type FormInst,
+  type FormRules,
+  NButton,
+  NCollapseTransition,
+  NDivider,
+  NForm,
+  NFormItem,
+  NInput,
+} from "naive-ui";
 import UniversalMinimalistWallet from "@/components/PaperWallets/UniversalMinimalistWallet.vue";
 import PaperWalletWrapper from "@/components/PaperWalletsWrapper/PaperWalletWrapper.vue";
 
 const secret = ref("");
 const address = ref("");
+
+const formRef = ref<FormInst | null>(null);
+
+const formValue = ref({
+  secret: "",
+  address: "",
+});
+
+const rules: FormRules = {
+  secret: [
+    {
+      required: true,
+      message: "Please input secret field",
+      trigger: ["input", "blur"],
+    },
+  ],
+  address: [
+    {
+      required: true,
+      message: "Please input address field",
+      trigger: ["input", "blur"],
+    },
+  ],
+};
+
+function handleGeneratePaperWallets() {
+  formRef.value?.validate((errors) => {
+    if (errors) {
+      return;
+    }
+    secret.value = formValue.value.secret;
+    address.value = formValue.value.address;
+  });
+}
+
+function handleKeydown(e: KeyboardEvent) {
+  if (e.key === "Enter") {
+    handleGeneratePaperWallets();
+  }
+}
 </script>
 <template>
-  <div class="paper-wallets">
-    <h1>Paper Wallets</h1>
-  </div>
+  <h1>Generate Paper Wallets</h1>
 
-  <n-space vertical>
-    <n-input
-      v-model:value="secret"
-      placeholder="Secret key"
-      type="textarea"
-      :autosize="{
-        minRows: 1,
-        maxRows: 3,
-      }"
-      maxlength="154"
-    />
-    <n-input
-      v-model:value="address"
-      placeholder="Address"
-      type="textarea"
-      :autosize="{
-        minRows: 1,
-        maxRows: 3,
-      }"
-      maxlength="154"
-    />
-  </n-space>
+  <n-form
+    @submit.prevent="handleGeneratePaperWallets"
+    :model="formValue"
+    :rules="rules"
+    ref="formRef"
+  >
+    <n-form-item label="Secret key" path="secret">
+      <n-input
+        v-model:value="formValue.secret"
+        placeholder="type secret key"
+        type="textarea"
+        :autosize="{
+          minRows: 1,
+          maxRows: 3,
+        }"
+        @keydown.enter.space.prevent="handleKeydown"
+        maxlength="154"
+      />
+    </n-form-item>
+    <n-form-item label="Address" path="address">
+      <n-input
+        v-model:value="formValue.address"
+        placeholder="Type address"
+        type="textarea"
+        @keydown.enter.space.prevent="handleKeydown"
+        :autosize="{
+          minRows: 1,
+          maxRows: 3,
+        }"
+        maxlength="154"
+      />
+    </n-form-item>
+    <n-button type="primary" attr-type="submit">
+      Generate paper wallets
+    </n-button>
+  </n-form>
 
   <n-divider />
   <div class="print-block">
-    <PaperWalletWrapper>
-      <UniversalMinimalistWallet :secret="secret" :address="address" />
-    </PaperWalletWrapper>
+    <n-collapse-transition :show="!!(secret && address)">
+      <PaperWalletWrapper>
+        <UniversalMinimalistWallet :secret="secret" :address="address" />
+      </PaperWalletWrapper>
+    </n-collapse-transition>
   </div>
 </template>
