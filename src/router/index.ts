ce9cfa7787bfbc1f03c2b6a7e6fc4e8156e46d38
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2023-04-21 01:17:47 +0300

    fix homepage

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: import("../views/HomeView.vue"),
+      component: () => import("../views/HomeView.vue"),
     },
     {
       path: "/create-wallet",
