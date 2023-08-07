cf91f13c0fe7720cb7af2248018945cddabdf247
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2023-08-08 00:06:33 +0300

    Feature/fix ios (#20)
    * fix typo
    
    * fix iOS canvas render

--- src/entities/PaperWallets/lib/download/downloadHtmlAsJpeg.ts
+++ src/entities/PaperWallets/lib/download/downloadHtmlAsJpeg.ts
@@ -1,13 +1,23 @@
-import { toJpeg } from "html-to-image";
+import { toCanvas } from "html-to-image";
 import downloadFileHandler from "@/entities/PaperWallets/lib/download/downloadFileHandler";
 
-export default function downloadHtmlAsJpeg(
+export default async function downloadHtmlAsJpeg(
   targetElement: HTMLElement,
   filename: string
 ) {
-  return toJpeg(targetElement, { quality: 0.95 })
-    .then(downloadFileHandler(`${filename}.jpeg`))
-    .catch(() => {
-      throw "Problem to convert to JPEG";
-    });
+  try {
+    const canvas = await toCanvas(targetElement);
+    await new Promise((resolve) => setTimeout(resolve, 1));
+
+    const dataURL = canvas.toDataURL("image/jpeg", 1);
+    downloadFileHandler(`${filename}.png`)(dataURL);
+  } catch {
+    throw "Problem to convert to JPEG";
+  }
+  // todo https://github.com/bubkoo/html-to-image/issues/420
+  // return toJpeg(targetElement, { quality: 0.95 })
+  //   .then(downloadFileHandler(`${filename}.jpeg`))
+  //   .catch(() => {
+  //     throw "Problem to convert to JPEG";
+  //   });
 }
--- src/entities/PaperWallets/lib/download/downloadHtmlAsPng.ts
+++ src/entities/PaperWallets/lib/download/downloadHtmlAsPng.ts
@@ -1,13 +1,23 @@
-import { toPng } from "html-to-image";
+import { toCanvas } from "html-to-image";
 import downloadFileHandler from "@/entities/PaperWallets/lib/download/downloadFileHandler";
 
-export default function downloadHtmlAsPng(
+export default async function downloadHtmlAsPng(
   targetElement: HTMLElement,
   filename: string
 ) {
-  return toPng(targetElement)
-    .then(downloadFileHandler(`${filename}.png`))
-    .catch(() => {
-      throw "Problem to convert to PNG";
-    });
+  try {
+    const canvas = await toCanvas(targetElement);
+    await new Promise((resolve) => setTimeout(resolve, 1));
+
+    const dataURL = canvas.toDataURL();
+    downloadFileHandler(`${filename}.png`)(dataURL);
+  } catch {
+    throw "Problem to convert to PNG";
+  }
+  // todo https://github.com/bubkoo/html-to-image/issues/420
+  // return toPng(targetElement)
+  //   .then(downloadFileHandler(`${filename}.png`))
+  //   .catch(() => {
+  //     throw "Problem to convert to PNG";
+  //   });
 }
