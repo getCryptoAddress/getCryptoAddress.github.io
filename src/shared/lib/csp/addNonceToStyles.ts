bb94782ebbf180135417c7af57c81da6ec8fae8d
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2024-11-04 12:41:56 +0200

    Add nonce instead of unsafe-inline

---
+++ src/shared/lib/csp/addNonceToStyles.ts
@@ -0,0 +1,61 @@
+const originalAppendChild = Node.prototype.appendChild;
+const originalInsertBefore = Node.prototype.insertBefore;
+
+const assetsFolder =
+  import.meta.env.MODE === "production"
+    ? "/assets/"
+    : "/node_modules/.vite/deps/";
+
+/**
+ * Check if the current chunk is one of those that do not support nonce
+ *
+ * [tag-nonce] Search by tag in the code
+ */
+function isSelectedChunk(chunksWithoutNonce: string[]) {
+  const { stack } = new Error();
+  const pathOfChunks = chunksWithoutNonce.map(
+    (chunk) => window.location.origin + assetsFolder + chunk,
+  );
+
+  return !!stack
+    ?.split("\n")
+    .map((line) => line.match(/at.*\((http.+?)\)$/)?.[1])
+    .filter((line) => line && pathOfChunks.some((path) => line.includes(path)))
+    .length;
+}
+
+/**
+ * Add nonce to style elements that do not have it yet for the selected chunks
+ */
+export function addNonceToStyles(nonce: string, chunksWithoutNonce: string[]) {
+  const addNonceToStyle = (element: Node) => {
+    const isStyleElement = element instanceof HTMLStyleElement;
+    if (!isStyleElement) {
+      return;
+    }
+
+    const hasNonce = element.hasAttribute("nonce");
+    if (hasNonce) {
+      return;
+    }
+
+    if (!isSelectedChunk(chunksWithoutNonce)) {
+      return;
+    }
+
+    element.setAttribute("nonce", nonce);
+  };
+
+  Node.prototype.appendChild = function <T extends Node>(node: T): T {
+    addNonceToStyle(node);
+    return originalAppendChild.call(this, node) as T;
+  };
+
+  Node.prototype.insertBefore = function <T extends Node>(
+    node: T,
+    referenceNode: Node | null,
+  ): T {
+    addNonceToStyle(node);
+    return originalInsertBefore.call(this, node, referenceNode) as T;
+  };
+}
