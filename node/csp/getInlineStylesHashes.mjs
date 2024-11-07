80d8326365b2f20024d510bfe3dafdf2f487e09b
getCryptoAddress <125769450+getCryptoAddress@users.noreply.github.com>
2024-11-08 01:33:55 +0200

    Add hash instead of unsafe-inline for naive-ui (#33)
    * Add hash instead of unsafe-inline for naive-ui
    
    * Migrate GA to node 20
    
    * Check errors in browser's console

---
+++ node/csp/getInlineStylesHashes.mjs
@@ -0,0 +1,46 @@
+import crypto from "crypto";
+
+/**
+ * Calculates SHA-256 hash of the given style content and returns it in base64 format.
+ *
+ * ```
+ * # Input
+ * max-width:250px;text-align:left;margin:0 auto;width:100%;
+ * # output
+ * sha256-O5IIiIzIB9wS0DmNOhwTAp7C6vPXN8QJ3R0ZS+HTUNM=
+ * ```
+ *
+ * @param {string} styleContent
+ * @returns {string}
+ */
+function calculateStyleHash(styleContent) {
+  const hash = crypto
+    .createHash("sha256")
+    .update(styleContent, "utf8")
+    .digest("base64");
+  return `sha256-${hash}`;
+}
+
+/**
+ * Extracts inline styles from the given HTML content.
+ *
+ * @param {string} appHtml - The HTML content to extract inline styles from.
+ * @returns {string[]} An array of inline style strings.
+ */
+function getInlineStyles(appHtml) {
+  return (
+    appHtml
+      .match(/ style=".*?"/g)
+      ?.map((line) => line.replace(/^ style="/, "").replace(/"$/, "")) || []
+  );
+}
+
+/**
+ * Generates an array of unique SHA-256 hashes for all inline styles found in the given HTML content.
+ *
+ * @param {string} appHtml - The HTML content to extract and hash inline styles from.
+ * @returns {string[]} An array of unique SHA-256 hashes in base64 format.
+ */
+export function getInlineStylesHashes(appHtml) {
+  return [...new Set(getInlineStyles(appHtml).map(calculateStyleHash))];
+}
--- prerender.mjs
+++ prerender.mjs
@@ -1,6 +1,8 @@
 import fs from "node:fs";
 import path from "node:path";
 import { createServer } from "vite";
+import { addInlineStylesHashesToHtml } from "./node/csp/addInlineStylesHashesToHtml.mjs";
+import { getInlineStylesHashes } from "./node/csp/getInlineStylesHashes.mjs";
 import generateSitemap from "./node/sitemap/generateSitemap.mjs";
 
 // todo refactor file, separate into functions
@@ -21,7 +23,12 @@ generateSitemap(routerPaths, "https://getcryptoaddress.github.io", "dist");
 for (const routerPath of routerPaths) {
   const { appHtml, ctx } = await render(routerPath);
 
-  const pageHtml = template
+  let pageHtml = template;
+
+  const styleHashes = getInlineStylesHashes(appHtml);
+  pageHtml = addInlineStylesHashesToHtml(pageHtml, styleHashes);
+
+  pageHtml = pageHtml
     .replace("<!--app-head-->", ctx?.teleports?.head || "")
     .replace("<!--app-html-->", appHtml)
     .replace(/<!--.*?-->/g, "")
@@ -32,7 +39,6 @@ for (const routerPath of routerPaths) {
     recursive: true,
   });
   fs.writeFileSync(path.join(pageFolder, "index.html"), pageHtml);
-  console.log("Generated:", path.join(pageFolder, "index.html"));
   await new Promise((resolve) => setTimeout(resolve, 300));
 }
 await vite.close();
